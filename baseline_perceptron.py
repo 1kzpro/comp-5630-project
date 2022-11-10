e61add83d1d3a3b1432bd5aec895e80ef085725a
Kazybek Mizam <kazymizam@mail.ru>
2022-11-10 14:05:44 -0600

    add perceptron baseline test

---
+++ baseline_perceptron.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Perceptron
+import pandas as pd
+import numpy as np
+
+raw_df = pd.read_csv("data/bank-full.csv", sep=";")
+
+for column in raw_df.columns:
+    column_dtype = raw_df[column].dtype
+    if str(column_dtype) == "object":
+        # print(f"Column: {column} and type {column_dtype}")
+        dummy = pd.get_dummies(raw_df[column], prefix=column, drop_first=True)
+        raw_df = pd.concat([raw_df, dummy], axis=1).drop(column, axis=1)
+
+print("Pre-Processed Training Dataset", raw_df)
+
+X = raw_df.iloc[:,:-1]
+y = raw_df.iloc[:,-1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+# print(X_train_r)
+# print(y_train_r)
+
+clf = Perceptron()
+
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+
+print("Perceptron Accuracy: ", accuracy)
